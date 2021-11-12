87ec59698e9c2fd82f27edd7784a3edf372af731
Mikhail Bibochkin <bibockinm@gmail.com>
2021-11-12 17:54:12 +0500

    add get and post request

--- src/rsc/Form.js
+++ src/rsc/Form.js
@@ -1,17 +1,46 @@
 import React from "react";
 
 class Form extends React.Component {
+    submitForm (event) {
+        event.preventDefault();
+
+        const userName = document.getElementById('usernamesignup'),
+            password = document.getElementById('passwordsignup');
+
+        let message = "email=" + encodeURIComponent(userName.value) + "&password=" + encodeURIComponent(password.value),
+            xhrPost = new XMLHttpRequest(),
+            xhrGet = new XMLHttpRequest(),
+            userID = '';
+
+        xhrPost.open("POST", "https://mysterious-reef-29460.herokuapp.com/api/v1/validate", true);
+        xhrPost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhrPost.send(message);
+        xhrPost.onreadystatechange = () => {
+            if (xhrPost.readyState === 4 && xhrPost.status === 200) {
+                userID = JSON.parse(xhrPost.responseText).data.id;
+                xhrGet.open("GET", "https://mysterious-reef-29460.herokuapp.com/api/v1/user-info/" + userID, true);
+                xhrGet.onreadystatechange = () => {
+                    if (xhrGet.readyState === 4 && xhrGet.status === 200) {
+                        console.dir(JSON.parse(xhrGet.responseText));
+                    }
+                }
+                xhrGet.send(); 
+            }
+        };
+        
+    }
+
     render () {
         return(
-        <div className="loginform">
-            <form>
-                <label htmlFor="usernamesignup">Логин</label>
-                <input id="usernamesignup" name="usernamesignup" required="required" type="text" className="input-text"></input>
-                <label htmlFor="passwordsignup">Пароль</label>
-                <input id="passwordsignup" name="passwordsignup" required="required" type="password" className="input-text"></input>
-                <input id="submitsignup" type="submit" value="Отправить"></input>
-            </form>
-        </div>
+            <div className="loginform">
+                <form>
+                    <label htmlFor="usernamesignup">Логин</label>
+                    <input id="usernamesignup" name="usernamesignup" required="required" type="text" className="input-text"></input>
+                    <label htmlFor="passwordsignup">Пароль</label>
+                    <input id="passwordsignup" name="passwordsignup" required="required" type="password" className="input-text"></input>
+                    <input id="submitsignup" type="submit" value="Отправить" onClick={this.submitForm.bind(this)}></input>
+                </form>
+            </div>
         );
     }
 };
